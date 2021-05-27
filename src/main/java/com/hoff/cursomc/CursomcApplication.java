726034ec6bfd000ae22cf856e667dad65af98a79
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-05-27 08:51:27 -0300

    feat: domain e repositories de pagamentos e pedido

--- src/main/java/com/hoff/cursomc/CursomcApplication.java
+++ src/main/java/com/hoff/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.hoff.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.hoff.cursomc.domain.Cidade;
 import com.hoff.cursomc.domain.Cliente;
 import com.hoff.cursomc.domain.Endereco;
 import com.hoff.cursomc.domain.Estado;
+import com.hoff.cursomc.domain.Pagamento;
+import com.hoff.cursomc.domain.PagamentoComBoleto;
+import com.hoff.cursomc.domain.PagamentoComCartao;
+import com.hoff.cursomc.domain.Pedido;
 import com.hoff.cursomc.domain.Produto;
+import com.hoff.cursomc.domain.enums.EstadoPagamento;
 import com.hoff.cursomc.domain.enums.TipoCliente;
 import com.hoff.cursomc.repositories.CategoriaRepository;
 import com.hoff.cursomc.repositories.CidadeRepository;
 import com.hoff.cursomc.repositories.ClienteRepository;
 import com.hoff.cursomc.repositories.EnderecoRepository;
 import com.hoff.cursomc.repositories.EstadoRepository;
+import com.hoff.cursomc.repositories.PagamentoRepository;
+import com.hoff.cursomc.repositories.PedidoRepository;
 import com.hoff.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -85,6 +97,22 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(en1, en2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, en1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:32"), cli1, en2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENDTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 	
--- src/main/java/com/hoff/cursomc/domain/Cliente.java
+++ src/main/java/com/hoff/cursomc/domain/Cliente.java
@@ -17,11 +17,10 @@ import javax.persistence.OneToMany;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.hoff.cursomc.domain.enums.TipoCliente;
 
-
 @Entity
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -29,17 +28,20 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
+
 	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-	
+
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+
 	public Cliente() {
-		
+
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -106,6 +108,15 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -131,9 +142,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
+
 }
