53b3f3bcb49b76d43e81965af1e05de9b48b685e
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2021-06-21 17:23:12 -0300

    feat: bean validation categoria

--- src/main/java/com/hoff/cursomc/services/CategoriaService.java
+++ src/main/java/com/hoff/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.hoff.cursomc.domain.Categoria;
+import com.hoff.cursomc.dto.CategoriaDTO;
 import com.hoff.cursomc.repositories.CategoriaRepository;
 import com.hoff.cursomc.services.exceptions.DataIntegrityException;
 import com.hoff.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,4 +58,8 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	} 
 }
